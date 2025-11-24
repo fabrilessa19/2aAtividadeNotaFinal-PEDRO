5e660da2bd5c89ddb4844d689ffd2c1484921aae
fabrilessa19 <pedrobraboshiny@gmail.com>
2025-11-24 20:42:44 -0300

    database.php

--- QUESTÃO-2/database.php
+++ QUESTÃO-2/database.php
@@ -1,1 +1,20 @@
+<?php
+$arquivoBanco = __DIR__ . '/tarefas.sqlite';
+
+try {
+    $conexao = new PDO("sqlite:" . $arquivoBanco);
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $conexao->exec("
+        CREATE TABLE IF NOT EXISTS tarefas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            descricao TEXT NOT NULL,
+            data_vencimento TEXT,
+            concluida INTEGER DEFAULT 0
+        );
+    ");
+
+} catch (PDOException $erro) {
+    die("Erro no banco: " . $erro->getMessage());
+}
 
