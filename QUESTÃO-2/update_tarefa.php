9f06592a533ead471a50ce853ad005ae87afb7a7
fabrilessa19 <pedrobraboshiny@gmail.com>
2025-11-24 20:44:40 -0300

    update_tarefa.php

--- QUESTÃO-2/update_tarefa.php
+++ QUESTÃO-2/update_tarefa.php
@@ -1,1 +1,13 @@
+<?php
+require_once "database.php";
+
+$idTarefa = $_POST["id"] ?? "";
+
+if (ctype_digit($idTarefa)) {
+    $sql = $conexao->prepare("UPDATE tarefas SET concluida = 1 WHERE id = :id");
+    $sql->execute([":id" => $idTarefa]);
+}
+
+header("Location: index.php");
+exit;
 
